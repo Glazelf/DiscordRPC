d14bcd54dc4a57259958b7544cea37951718e12e
Glazelf <gideon.steenbergen@gmail.com>
2021-06-01 17:51:39 +0200

    init winforms project

---
+++ DiscordRPC.WinForms/Main.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace DiscordRPC.WinForms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
