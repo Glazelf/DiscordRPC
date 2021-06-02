f0db7b70708741a90357da94debef47684cc2351
Glazelf <gideon.steenbergen@gmail.com>
2021-06-02 19:13:10 +0200

    activity class

---
+++ DiscordRPC.Core/Util/Activity.cs
@@ -0,0 +1,23 @@
+﻿namespace DiscordRPC.Core
+{
+    public class Activity
+    {
+        public static bool UpdateActivity(Discord.Discord discord, Discord.Activity activity)
+        {
+            var activityManager = discord.GetActivityManager();
+            bool returnBool = false;
+            activityManager.UpdateActivity(activity, (result) =>
+            {
+                if (result == Discord.Result.Ok)
+                {
+                    returnBool = true;
+                }
+                else
+                {
+                    returnBool = false;
+                }
+            });
+            return returnBool;
+        }
+    }
+}
