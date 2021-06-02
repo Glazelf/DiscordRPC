f6ea1f3e47a3b5d5f1f7e6ea8d79144ffcb1c43e
Glazelf <gideon.steenbergen@gmail.com>
2021-06-02 22:00:13 +0200

    layout additions

--- DiscordRPC.WinForms/Main.Designer.cs
+++ DiscordRPC.WinForms/Main.Designer.cs
@@ -29,33 +29,40 @@ namespace DiscordRPC.WinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.ConnectButon = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
             this.ClientIDNumeric = new System.Windows.Forms.NumericUpDown();
             this.ClientIDLabel = new System.Windows.Forms.Label();
             this.RPCTabControl = new System.Windows.Forms.TabControl();
             this.RPCSettingsTab = new System.Windows.Forms.TabPage();
             this.RPCLogTab = new System.Windows.Forms.TabPage();
             this.LogText = new System.Windows.Forms.RichTextBox();
+            this.ConnectionGroup = new System.Windows.Forms.GroupBox();
+            this.StatusGroup = new System.Windows.Forms.GroupBox();
+            this.StatusConnectionText = new System.Windows.Forms.Label();
+            this.StatusConnectionLabel = new System.Windows.Forms.Label();
+            this.SyncButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ClientIDNumeric)).BeginInit();
             this.RPCTabControl.SuspendLayout();
             this.RPCLogTab.SuspendLayout();
+            this.ConnectionGroup.SuspendLayout();
+            this.StatusGroup.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ConnectButon
+            // ConnectButton
             // 
-            this.ConnectButon.AccessibleName = "ConnectButon";
-            this.ConnectButon.Location = new System.Drawing.Point(713, 415);
-            this.ConnectButon.Name = "ConnectButon";
-            this.ConnectButon.Size = new System.Drawing.Size(75, 23);
-            this.ConnectButon.TabIndex = 0;
-            this.ConnectButon.Text = "Connect";
-            this.ConnectButon.UseVisualStyleBackColor = true;
-            this.ConnectButon.Click += new System.EventHandler(this.ConnectButton_Click);
+            this.ConnectButton.AccessibleName = "ConnectButton";
+            this.ConnectButton.Location = new System.Drawing.Point(25, 64);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
             // 
             // ClientIDNumeric
             // 
             this.ClientIDNumeric.AccessibleName = "ClientIDNumeric";
-            this.ClientIDNumeric.Location = new System.Drawing.Point(658, 355);
+            this.ClientIDNumeric.Location = new System.Drawing.Point(64, 37);
             this.ClientIDNumeric.Maximum = new decimal(new int[] {
             -1486618625,
             232830643,
@@ -70,7 +77,7 @@ namespace DiscordRPC.WinForms
             // 
             this.ClientIDLabel.AccessibleName = "ClientIDLabel";
             this.ClientIDLabel.AutoSize = true;
-            this.ClientIDLabel.Location = new System.Drawing.Point(597, 357);
+            this.ClientIDLabel.Location = new System.Drawing.Point(3, 39);
             this.ClientIDLabel.Name = "ClientIDLabel";
             this.ClientIDLabel.Size = new System.Drawing.Size(55, 15);
             this.ClientIDLabel.TabIndex = 3;
@@ -84,7 +91,7 @@ namespace DiscordRPC.WinForms
             this.RPCTabControl.Location = new System.Drawing.Point(0, 0);
             this.RPCTabControl.Name = "RPCTabControl";
             this.RPCTabControl.SelectedIndex = 0;
-            this.RPCTabControl.Size = new System.Drawing.Size(788, 349);
+            this.RPCTabControl.Size = new System.Drawing.Size(528, 349);
             this.RPCTabControl.TabIndex = 4;
             // 
             // RPCSettingsTab
@@ -93,7 +100,7 @@ namespace DiscordRPC.WinForms
             this.RPCSettingsTab.Location = new System.Drawing.Point(4, 24);
             this.RPCSettingsTab.Name = "RPCSettingsTab";
             this.RPCSettingsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.RPCSettingsTab.Size = new System.Drawing.Size(780, 321);
+            this.RPCSettingsTab.Size = new System.Drawing.Size(520, 321);
             this.RPCSettingsTab.TabIndex = 1;
             this.RPCSettingsTab.Text = "Settings";
             this.RPCSettingsTab.UseVisualStyleBackColor = true;
@@ -105,7 +112,7 @@ namespace DiscordRPC.WinForms
             this.RPCLogTab.Location = new System.Drawing.Point(4, 24);
             this.RPCLogTab.Name = "RPCLogTab";
             this.RPCLogTab.Padding = new System.Windows.Forms.Padding(3);
-            this.RPCLogTab.Size = new System.Drawing.Size(780, 321);
+            this.RPCLogTab.Size = new System.Drawing.Size(520, 321);
             this.RPCLogTab.TabIndex = 2;
             this.RPCLogTab.Text = "Log";
             this.RPCLogTab.UseVisualStyleBackColor = true;
@@ -115,39 +122,106 @@ namespace DiscordRPC.WinForms
             this.LogText.AccessibleName = "LogText";
             this.LogText.Location = new System.Drawing.Point(0, 0);
             this.LogText.Name = "LogText";
-            this.LogText.Size = new System.Drawing.Size(780, 321);
+            this.LogText.Size = new System.Drawing.Size(524, 325);
             this.LogText.TabIndex = 0;
-            this.LogText.Text = "";
+            this.LogText.Text = "Booted application.";
+            // 
+            // ConnectionGroup
+            // 
+            this.ConnectionGroup.AccessibleName = "ConnectionGroup";
+            this.ConnectionGroup.Controls.Add(this.SyncButton);
+            this.ConnectionGroup.Controls.Add(this.ClientIDNumeric);
+            this.ConnectionGroup.Controls.Add(this.ConnectButton);
+            this.ConnectionGroup.Controls.Add(this.ClientIDLabel);
+            this.ConnectionGroup.Location = new System.Drawing.Point(324, 351);
+            this.ConnectionGroup.Name = "ConnectionGroup";
+            this.ConnectionGroup.Size = new System.Drawing.Size(200, 100);
+            this.ConnectionGroup.TabIndex = 5;
+            this.ConnectionGroup.TabStop = false;
+            this.ConnectionGroup.Text = "Connection";
+            // 
+            // StatusGroup
+            // 
+            this.StatusGroup.AccessibleName = "StatusGroup";
+            this.StatusGroup.Controls.Add(this.StatusConnectionText);
+            this.StatusGroup.Controls.Add(this.StatusConnectionLabel);
+            this.StatusGroup.Location = new System.Drawing.Point(4, 351);
+            this.StatusGroup.Name = "StatusGroup";
+            this.StatusGroup.Size = new System.Drawing.Size(314, 100);
+            this.StatusGroup.TabIndex = 6;
+            this.StatusGroup.TabStop = false;
+            this.StatusGroup.Text = "Status";
+            // 
+            // StatusConnectionText
+            // 
+            this.StatusConnectionText.AccessibleName = "StatusConnectionText";
+            this.StatusConnectionText.AutoSize = true;
+            this.StatusConnectionText.ForeColor = System.Drawing.Color.Red;
+            this.StatusConnectionText.Location = new System.Drawing.Point(86, 19);
+            this.StatusConnectionText.Name = "StatusConnectionText";
+            this.StatusConnectionText.Size = new System.Drawing.Size(79, 15);
+            this.StatusConnectionText.TabIndex = 8;
+            this.StatusConnectionText.Text = "Disconnected";
+            // 
+            // StatusConnectionLabel
+            // 
+            this.StatusConnectionLabel.AccessibleName = "StatusConnectionLabel";
+            this.StatusConnectionLabel.AutoSize = true;
+            this.StatusConnectionLabel.Location = new System.Drawing.Point(8, 19);
+            this.StatusConnectionLabel.Name = "StatusConnectionLabel";
+            this.StatusConnectionLabel.Size = new System.Drawing.Size(72, 15);
+            this.StatusConnectionLabel.TabIndex = 7;
+            this.StatusConnectionLabel.Text = "Connection:";
+            // 
+            // SyncButton
+            // 
+            this.SyncButton.AccessibleName = "SyncButton";
+            this.SyncButton.Enabled = false;
+            this.SyncButton.Location = new System.Drawing.Point(106, 64);
+            this.SyncButton.Name = "SyncButton";
+            this.SyncButton.Size = new System.Drawing.Size(75, 23);
+            this.SyncButton.TabIndex = 4;
+            this.SyncButton.Text = "Sync";
+            this.SyncButton.UseVisualStyleBackColor = true;
+            this.SyncButton.Click += new System.EventHandler(this.SyncButton_Click);
             // 
             // Main
             // 
+            this.AccessibleName = "Main";
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.Controls.Add(this.StatusGroup);
+            this.Controls.Add(this.ConnectionGroup);
             this.Controls.Add(this.RPCTabControl);
-            this.Controls.Add(this.ClientIDLabel);
-            this.Controls.Add(this.ClientIDNumeric);
-            this.Controls.Add(this.ConnectButon);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "DiscordRPC";
             this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ClientIDNumeric)).EndInit();
             this.RPCTabControl.ResumeLayout(false);
             this.RPCLogTab.ResumeLayout(false);
+            this.ConnectionGroup.ResumeLayout(false);
+            this.ConnectionGroup.PerformLayout();
+            this.StatusGroup.ResumeLayout(false);
+            this.StatusGroup.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button ConnectButon;
+        private System.Windows.Forms.Button ConnectButton;
         private System.Windows.Forms.NumericUpDown ClientIDNumeric;
         private System.Windows.Forms.Label ClientIDLabel;
         private System.Windows.Forms.TabControl RPCTabControl;
         private System.Windows.Forms.TabPage RPCSettingsTab;
         private System.Windows.Forms.TabPage RPCLogTab;
         private System.Windows.Forms.RichTextBox LogText;
+        private System.Windows.Forms.GroupBox ConnectionGroup;
+        private System.Windows.Forms.GroupBox StatusGroup;
+        private System.Windows.Forms.Label StatusConnectionText;
+        private System.Windows.Forms.Label StatusConnectionLabel;
+        private System.Windows.Forms.Button SyncButton;
     }
 }
 
--- DiscordRPC.WinForms/Main.cs
+++ DiscordRPC.WinForms/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace DiscordRPC.WinForms
@@ -10,6 +11,8 @@ namespace DiscordRPC.WinForms
             InitializeComponent();
 
         }
+
+        bool ConnectedBool = false;
         long ClientID = Settings.Default.ClientID;
 
         private void Main_Load(object sender, EventArgs e)
@@ -19,34 +22,50 @@ namespace DiscordRPC.WinForms
 
         private void ConnectButton_Click(object sender, System.EventArgs e)
         {
-            /*
-            Grab that Client ID from earlier
-            Discord.CreateFlags.Default will require Discord to be running for the game to work
-            If Discord is not running, it will:
-            1. Close your game
-            2. Open Discord
-            3. Attempt to re-open your game
-            Step 3 may fail when running directly from your editor
-            Therefore, always keep Discord running during tests, or use Discord.CreateFlags.NoRequireDiscord
-            */
-            var discord = new Discord.Discord(ClientID, (UInt64)Discord.CreateFlags.Default);
-
-            var activity = new Discord.Activity
+            if (ConnectedBool == false)
             {
-                State = "gay test",
-                Details = "wowzer",
-                Timestamps =
+                /*
+                Grab that Client ID from earlier
+                Discord.CreateFlags.Default will require Discord to be running for the game to work
+                If Discord is not running, it will:
+                1. Close your game
+                2. Open Discord
+                3. Attempt to re-open your game
+                Step 3 may fail when running directly from your editor
+                Therefore, always keep Discord running during tests, or use Discord.CreateFlags.NoRequireDiscord
+                */
+                var discord = new Discord.Discord(ClientID, (UInt64)Discord.CreateFlags.Default);
+
+                var activity = new Discord.Activity
+                {
+                    State = "gay test",
+                    Details = "wowzer",
+                    Timestamps =
                 {
                     Start = 5,
                 },
-            };
-            bool ActivityReturn = Core.Activity.UpdateActivity(discord, activity);
-            if(ActivityReturn == true)
-            {
-                LogText.Text += Environment.NewLine + "Successfully connected.";
-            } else
-            {
-                LogText.Text += Environment.NewLine + "Failed to connect.";
+                };
+                bool ActivityReturn = Core.Activity.UpdateActivity(discord, activity);
+                if (ActivityReturn == true)
+                {
+                    SyncButton.Enabled = true;
+                    ClientIDNumeric.Enabled = false;
+                    StatusConnectionText.ForeColor = Color.Green;
+                    StatusConnectionText.Text = "Connected";
+                    ConnectButton.Text = "Disconnect";
+                    ConnectedBool = true;
+                    LogText.Text += Environment.NewLine + "Successfully connected.";
+                }
+                else
+                {
+                    SyncButton.Enabled = false;
+                    ClientIDNumeric.Enabled = true;
+                    StatusConnectionText.ForeColor = Color.Red;
+                    StatusConnectionText.Text = "Disconnected";
+                    ConnectButton.Text = "Connect";
+                    ConnectedBool = false;
+                    LogText.Text += Environment.NewLine + "Failed to connect.";
+                }
             }
         }
 
@@ -54,6 +73,11 @@ namespace DiscordRPC.WinForms
         {
             Settings.Default.ClientID = (long)ClientIDNumeric.Value;
             Settings.Default.Save();
+        }
+
+        private void SyncButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
