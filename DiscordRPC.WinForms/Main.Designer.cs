1e5297e618e29589b3306542d822eec2f40d5661
Glazelf <gideon.steenbergen@gmail.com>
2021-06-02 15:48:55 +0200

    add first few buttons and forms

--- DiscordRPC.WinForms/Main.Designer.cs
+++ DiscordRPC.WinForms/Main.Designer.cs
@@ -29,13 +29,69 @@ namespace DiscordRPC.WinForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ConnectButon = new System.Windows.Forms.Button();
+            this.ClientIDNumeric = new System.Windows.Forms.NumericUpDown();
+            this.ClientIDLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientIDNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConnectButon
+            // 
+            this.ConnectButon.AccessibleName = "ConnectButon";
+            this.ConnectButon.Location = new System.Drawing.Point(713, 415);
+            this.ConnectButon.Name = "ConnectButon";
+            this.ConnectButon.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButon.TabIndex = 0;
+            this.ConnectButon.Text = "Connect";
+            this.ConnectButon.UseVisualStyleBackColor = true;
+            this.ConnectButon.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ClientIDNumeric
+            // 
+            this.ClientIDNumeric.AccessibleName = "ClientIDNumeric";
+            this.ClientIDNumeric.Location = new System.Drawing.Point(668, 355);
+            this.ClientIDNumeric.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.ClientIDNumeric.Name = "ClientIDNumeric";
+            this.ClientIDNumeric.Size = new System.Drawing.Size(120, 23);
+            this.ClientIDNumeric.TabIndex = 2;
+            this.ClientIDNumeric.ValueChanged += new System.EventHandler(this.ClientIDNumeric_ValueChanged);
+            // 
+            // ClientIDLabel
+            // 
+            this.ClientIDLabel.AccessibleName = "ClientIDLabel";
+            this.ClientIDLabel.AutoSize = true;
+            this.ClientIDLabel.Location = new System.Drawing.Point(607, 357);
+            this.ClientIDLabel.Name = "ClientIDLabel";
+            this.ClientIDLabel.Size = new System.Drawing.Size(55, 15);
+            this.ClientIDLabel.TabIndex = 3;
+            this.ClientIDLabel.Text = "Client ID:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClientIDLabel);
+            this.Controls.Add(this.ClientIDNumeric);
+            this.Controls.Add(this.ConnectButon);
+            this.Name = "Main";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ClientIDNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConnectButon;
+        private System.Windows.Forms.NumericUpDown ClientIDNumeric;
+        private System.Windows.Forms.Label ClientIDLabel;
     }
 }
 
--- DiscordRPC.WinForms/Main.cs
+++ DiscordRPC.WinForms/Main.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace DiscordRPC.WinForms
 {
@@ -7,7 +8,34 @@ namespace DiscordRPC.WinForms
         public Main()
         {
             InitializeComponent();
+            
+        }
+        long ClientID = Settings.Default.ClientID;
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            ClientIDNumeric.Value = Settings.Default.ClientID;
         }
 
+        private void ConnectButton_Click(object sender, System.EventArgs e)
+        {
+            /*
+            Grab that Client ID from earlier
+            Discord.CreateFlags.Default will require Discord to be running for the game to work
+            If Discord is not running, it will:
+            1. Close your game
+            2. Open Discord
+            3. Attempt to re-open your game
+            Step 3 may fail when running directly from your editor
+            Therefore, always keep Discord running during tests, or use Discord.CreateFlags.NoRequireDiscord
+            */
+            var discord = new Discord.Discord(ClientID, (UInt64)Discord.CreateFlags.Default);
+        }
+
+        private void ClientIDNumeric_ValueChanged(object sender, EventArgs e)
+        {
+            Settings.Default.ClientID = (long)ClientIDNumeric.Value;
+            Settings.Default.Save();
+        }
     }
 }
