d19f3ef5c458dcd3a2f648f6c2f13940f3d6e9b6
Glazelf <gideon.steenbergen@gmail.com>
2021-06-07 13:23:15 +0200

    bunch of code restructuring and cleaning

--- DiscordRPC.WinForms/Main.cs
+++ DiscordRPC.WinForms/Main.cs
@@ -11,31 +11,25 @@ namespace DiscordRPC.WinForms
             InitializeComponent();
         }
 
-        bool ConnectedBool = false;
-        long ClientID = Settings.Default.ClientID;
+        Discord.Discord discord;
+        Discord.ActivityManager activityManager;
 
         private void Main_Load(object sender, EventArgs e)
         {
             ClientIDNumeric.Value = Settings.Default.ClientID;
         }
 
+        bool ConnectedBool = false;
+        long ClientID = Settings.Default.ClientID;
+
         private void ConnectButton_Click(object sender, System.EventArgs e)
         {
+            ClientID = (long)ClientIDNumeric.Value;
             if (ConnectedBool == false)
             {
-                /*
-                Grab that Client ID from earlier
-                Discord.CreateFlags.Default will require Discord to be running for the game to work
-                If Discord is not running, it will:
-                1. Close your game
-                2. Open Discord
-                3. Attempt to re-open your game
-                Step 3 may fail when running directly from your editor
-                Therefore, always keep Discord running during tests, or use Discord.CreateFlags.NoRequireDiscord
-                */
-                var discord = new Discord.Discord(ClientID, (UInt64)Discord.CreateFlags.Default);
-                var activityManager = discord.GetActivityManager();
-
+                discord = new Discord.Discord(ClientID, (ulong)Discord.CreateFlags.Default);
+                activityManager = discord.GetActivityManager();
+                discord.SetLogHook(Discord.LogLevel.Debug, LogProblemsFunction);
 
                 var activity = new Discord.Activity
                 {
@@ -52,27 +46,22 @@ namespace DiscordRPC.WinForms
                 {
                     if (result == Discord.Result.Ok)
                     {
-                        SyncButton.Enabled = true;
-                        ClientIDNumeric.Enabled = false;
-                        StatusConnectionText.ForeColor = Color.Green;
-                        StatusConnectionText.Text = "Connected";
-                        ConnectButton.Text = "Disconnect";
-                        ConnectedBool = true;
+                        ChangeStateConnected();
                         LogText.Text += Environment.NewLine + "Successfully connected.";
                         LogText.Text += Environment.NewLine + Convert.ToString(result);
                     }
                     else
                     {
-                        SyncButton.Enabled = false;
-                        ClientIDNumeric.Enabled = true;
-                        StatusConnectionText.ForeColor = Color.Red;
-                        StatusConnectionText.Text = "Disconnected";
-                        ConnectButton.Text = "Connect";
-                        ConnectedBool = false;
+                        ChangeStateDisconnected();
                         LogText.Text += Environment.NewLine + "Failed to connect.";
                         LogText.Text += Environment.NewLine + Convert.ToString(result);
                     }
                 });
+            } else
+            {
+                discord.Dispose();
+                ChangeStateDisconnected();
+                LogText.Text += Environment.NewLine + "Disconnected.";
             }
         }
 
@@ -85,6 +74,33 @@ namespace DiscordRPC.WinForms
         private void SyncButton_Click(object sender, EventArgs e)
         {
 
+        }
+        public void LogProblemsFunction(Discord.LogLevel level, string message)
+        {
+            LogText.Text += Environment.NewLine + $"Discord:{level} - {message}";
+        }
+
+        public void ChangeStateConnected()
+        {
+            ConnectButton.Enabled = false;
+            SyncButton.Enabled = true;
+            ClientIDNumeric.Enabled = false;
+            StatusConnectionText.ForeColor = Color.Green;
+            StatusConnectionText.Text = "Connected";
+            ConnectButton.Text = "Disconnect";
+            ConnectedBool = true;
+        }
+
+        public void ChangeStateDisconnected()
+        {
+            ConnectButton.Enabled = true;
+            SyncButton.Enabled = false;
+            ClientIDNumeric.Enabled = true;
+            StatusConnectionText.ForeColor = Color.Red;
+            StatusConnectionText.Text = "Disconnected";
+            ConnectButton.Text = "Connect";
+            ConnectedBool = false;
+            
         }
     }
 }
